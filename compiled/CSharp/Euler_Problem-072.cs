37f3a408222d195e103e4ff790e91a830c1e0ba5
Mike Schwörer <mailport@mikescher.de>
2015-08-21 17:54:39 +0200

    Added Solution to Problem [072]

---
+++ compiled/CSharp/Euler_Problem-072.cs
@@ -0,0 +1,225 @@
+/* compiled with BefunCompile v1.0.7 (c) 2015 */
+public static class Program 
+{
+private static readonly string _g = "Ah+LCAAAAAAABADNWHlYU9cSp0XBCARfXNgE5KsRi5GgoEjYFFo/JDQoWokICbuAKJsxxBgigkYLKBUSlrDIEwQUJEHQAGEXEMJWglIgbNKAEAkQhLCE5EHfP/a1Aq/a"+
+                                    "vnf/mDtz5nfm3Dsz58zcqxmxQWr5Wic1loz2244N/LZ8414c2kovUcVncKvWJirJRPtuyKaCKhmZUOewTRHf2HaNp+yqA4oyux+lnHX6fvFxkFT3vQNOGJ4G8NuwGf5w"+
+                                    "tb59CxMXND1OlIxLRAPm4uFRtEToZy5aHJ2ZGyYuditKMNM9IxtnGoV95SIuq1xEgdb/VBFFpL5sllKbUpX6/dVvGC/JteTJ9cheB1KMr3GBzkXSSL7UR1g5u/LqeUBT"+
+                                    "68CdzYlp1eraBpYkY4TXTSA2qVVenX8oTQXGCbDQv8QYDPd861+3EnA8PGiqIKZOw4t/fuZGA1gr27KtXfP/U/DOzjCQVsiZpgWg6ECe4t/L6H0YrC3OfGOzq0peBjTO"+
+                                    "FxCsFEu9IlD2IyzTLUcgklB335JeCbVmVuC1W0BjKq06wXMMF0Nwx69uWP6D16J9JSg7EiMc++e8tuJTZVYolb3Nzcg/qMsVInsRcTaSW9HkHUHfzjC6sI8ibZi5tzaw"+
+                                    "f6igBgnkJD1bATzEJLzRBeeeh0DDVkzqin5KrjfH/BsOnvzMCh5bw5egrfgR9BxobX7ZOta8K4S3LFTmlyn/KsBpv2qWBNC/NX8Ae2PCAnlTpy7ynzNe14fFRXGBHm5F"+
+                                    "lp1I/vGDFOPYj4sz7+p/szfJA8bo0HRMFPfwyv68f/7DWW/X92lFfJV3HlIXuuKLXyvkFXXMbSYQTrQVA89EcvW9nUiQWkFCwRPLbW5zNg4dWhH0q7tyedE1SLOY15iX"+
+                                    "rpDUYzyY+z5KSzpGPV+ZdW71aZYdHzxXayzTD/PO+Sk2viCGvW3c5W9n3K6Qs1JbrbvASOA6Fqvd1mv9J9AMisf5xHjFf4DTFkDeTw1o7a4QYwjdbDklgfAcxaUsza8G"+
+                                    "8aqXcvEd7cW8f9fnUG8PTMaPRtfgqHUoR94vlazaQFI+nZPmC8HuVmOpN3xcxM+4f5gh1xra55s7+vuo8JL00yTknaVdE1UhCEy0A4BpzKSHpywAEGjm1+TjX7JY3tbg"+
+                                    "2LWqbgfo/qjM2c9crYgssYfbrlzwMhqHLeWoB0SxSIWFrBUoU+mRHpC79P8ULl7odIz6pqse550evnHJI8z9aRZLDsmzyYpecteezTrpe2sFDZ+qwuxrtVztEMxvNuN1"+
+                                    "ZXML2gfbC9k/u0EpFQLENrgzTjtiyQMxNSBkb70rADLuTl9HweYjv2QZn4GEcpMhv4ftsqEbZICaj1iQPvvthPkAxNup47mjTqQMxR5sI/cglPs9gF3l5Yz7vqpATxWJ"+
+                                    "Iuvt/STVZstbsQ6aaH4Eb+WDVYoy8CXH3JeztsYC1OYSucVMdVvlKqUIv2vMnecqtu/ZXiF4FO7tYkBj6rg4KCtA8qzjtx2VZj0GPbzx06eoGlvTELnA4Swy7ZSyfiEs"+
+                                    "6cHpKjuXxvZR/zsOAPiNfScfVxv5gCFba20+G0he/Ynbs7x5+rAV3KlcFzIeTFf5s0TJ8Q2QIqcbIMtaH8cI416HcyoDK8F856J+EJbm6H/bGKnLeKG+AuzeptGzzXBk"+
+                                    "mao+EvXcyJ1u/Hlvr9npNxJuhMN2tmulAQEYqyxLC3XQaPRx0seGBg42ZvQe8cV64pUfJoYifly1U1HuFkAGk3xL3WUzGfqIA0iUZ5IPjdl46JUzLiHgUaVg/+CpMK5H"+
+                                    "bbYMixV5UoESp5KpCoH+Hujt1bkICyK9bhoKb7hXGZA3YFcZQW0CkMFhg6jqjOtKhtTNOnCZutKb8PXQiwe1T1Hk7+AV4jaiOL6ILIjKdqGq7lYmozPp+Jn9h/gHHHWC"+
+                                    "A54apeW7+3POM05NvPEMsvCo72d0+Gf313yGBabPZ6XSzKaNcz4Lxak3X7isPRQrWO0MX2a1XsWvZ6iRm4Nsc018EbmOb5Z6IZ7GwczTugHo61yMIvXknjgGtFJQTGrK"+
+                                    "JsM5cc64nuqh9gYwH05j8l2CfLggIRiJEtKMCgNvL4AgefifkxyKXhAAlA7TtZvTYgh1MMg9adeNXDbYVUVrCEA68Ztv7gDLj2hB/zeD1enTquw0vOpSc4AbwvUul5hI"+
+                                    "IaZLjtwEKmzbDehp0W+ZQkVyyS7MPDYqy5eSRT567G1ICi5FuFSh5AcJyTMWHk+/yTWwthBONU4xldxwHsrjvAIlHSrYJ+MEqDj1XMpw6VIVqw5GZ5Ye74Sc2BN1r3XR"+
+                                    "ZMpkvAaJUqldgI3daPC87eAdG84fvuGoFDbNZGpsOSG9iLUD2R3wBjTtlrZ8EnHk0a69aSoezi8x+6xepYMNHsjLF+nKZCcWhHvGhmp11Oz4PobSGrlf75di5XSfNlDi"+
+                                    "CZmbS6ZOfedgod9QEc1zjX50DO51VEU50En+vzbaalpooLcziqhoXbWJc9J4a76LqkQjNlyLkXlZx73aQGy6G2Dpbzt26uVNbxH+OOhmf0pXdrQCe1Z4f6d8pWnhSS21"+
+                                    "h5PjR44pX8f7ZFoctBnsC7unt07YjrjhG19bLgt22cDPTt7YqffJCyB/PaNfCA62W9/iXsgib2J1H9+jRXmedtoC4meZG47svekAoDMVCkGuOLWPA1EWGVDe05oGxGof"+
+                                    "Y9K/adPuzOGDvfs6BtV+gK26E/mUJjvOWAa4Uc3L4+L9hOiYjJMWoCGFgv2GrTYv69u+1gJMyTgkqHnt+sV9JDY6HCed26hgsDkw/5K1VRUz9PSQTCLA6AwMvNOl9/Ce"+
+                                    "IOmm8CRIMuient99clGopwWCggAcizdsbeAc1tfShmeG7/xMy1hR2S9j7r0e4P7wAFQ8qFtDuG8ct+eO9ds7JVuEoXzHJ4d00oz5dhzZ4qCiN1nhfAY8QPNt818F3gCz"+
+                                    "GlnFy8usdAKvNfndV92rdT9Ym1sm2Pyz3WqdZbpflRSW/jjG2/+XiDqRbC1N4IrlbLW6J7KojjmnUiIDSf0kkilT/9bMNsUSVhSigAz5goUUd8mxOq5xlWiR3LxS2tU1"+
+                                    "D55S4AN4qSakA4VGzxYOA4R6GU/0fw1iOD7eriMhIO6N2fEqU5tsXuNy4HYPaDxsx3hyMhiKsf6KJ0dh+suRtWaQvsYkq2C/5kuDOdWxxc2Ry6EH8V2snc7VOv3w581C"+
+                                    "nwFziPn4qnntEsUc4n7KdOi0bW2+GU1cj3wt+9pchTVfMa/tdkYTKXGH8KR5qZEeBOfFGHrOH4PVFCBkmc9KPXew+Om3Lax3rKF8p++tFKis4R+Q85/qoj4EwuxIQ+f+"+
+                                    "QZZbywbbe1brAt0o6h4sD3q7oJ0f0eQd245ExXURlgdVWTy7EujBzBN2bjgnH2HNkIG1DyQPPlb2c9LRY1FcTHaPEpW9O5vO1C1ccA1SBR+gdGRcRvsiHqTXCorXYGw0"+
+                                    "2UHmD36UXlPwl9jD03YoSkT8nunpxcmJS8IebJ+TrbmJesqTPo0UBJaUaW+fx1fE2taVZNpLZutS0U5ozbu2ddhMHpU9OeIPMyWUXTlkAzU24dhKa0gmXObZ1BJqnX1f"+
+                                    "eTFhcQ6a+tQvLHVWdClIODTpL5ogjdgKNVGlIRgi/qokOHgBr2ia3JvyUDFscAFtcyVh4hKjxKjXXIP/hN8HNQX+ZNIoKl2A7iFdTRIHSyYGz87dFvobDcVeXRA7X52H"+
+                                    "aqKJib3P2Wcli3HdIy1Cw7p6IQs6idB9Uf2e+rZnYHK4PM/sO3NivJhRRMQHiyZG6pQQ5T+FLIiwdw2FbL+achFhdvTVWJyfft61vMnHLZ3DhsI69OJlU4LKjFiUgPui"+
+                                    "t5g8Z0JoWxwSjwRP8b/lTtmj6tj2qVdg6n1JF80d+/rMEejLmuJDhNdHg1w0ykyZxmKR3K7uDgTb/ueOu5kl3YPvuTMHrj8Ooszr244Tt8zmjE++nxFPDqcWp/ZN3c1k"+
+                                    "vweW4YSXYKYSffbIe/GgH0oYHKI+HzExMWNAaCOIDAgBGMPFt+9nRVPd3dTBkZGBS0khwTDTkERxMDZIVBPn1ZJXEhIyQeVBFcvnalJDwAuw2a3li7LDd1OJM8+Serv8"+
+                                    "hnqfsLtn50pFog5b88Xcas0+olnyw74hHHT8GklC9DN7vDCibrrItWWjL5YXiYcuX1kgBgcZEjYkEUeCg8SV7Lq6d+8mFkxEd0pTUVPzLfZ5kpHR6cFXPPYr3hJFl3ub"+
+                                    "l8lFyT+em20aS5+2bRnvnDrXb0vSNHcjIud1eHOptWbfaYLY5re+Mze2QT+YLCreIpGl37oW/KhaVupf8/rlo0wYAAA=";
+private static readonly long[]  g = System.Array.ConvertAll(zd(System.Convert.FromBase64String(_g)),b=>(long)b);
+private static byte[]zd(byte[]o){byte[]d=System.Linq.Enumerable.ToArray(System.Linq.Enumerable.Skip(o, 1));for(int i=0;i<o[0];i++)d=zs(d);return d;}
+private static byte[]zs(byte[]o){using(var c=new System.IO.MemoryStream(o))
+                                 using(var z=new System.IO.Compression.GZipStream(c,System.IO.Compression.CompressionMode.Decompress))
+                                 using(var r=new System.IO.MemoryStream()){z.CopyTo(r);return r.ToArray();}}
+private static long gr(long x,long y){return(x>=0&&y>=0&&x<486&&y<1047)?g[y*486+x]:0;}
+private static void gw(long x,long y,long v){if(x>=0&&y>=0&&x<486&&y<1047)g[y*486+x]=v;}
+private static System.Collections.Generic.Stack<long> s=new System.Collections.Generic.Stack<long>();
+private static long sp(){ return (s.Count==0)?0:s.Pop(); }
+private static void sa(long v){ s.Push(v); }
+private static long sr(){ return (s.Count==0)?0:s.Peek(); }
+private static long td(long a,long b){ return (b==0)?0:(a/b); }
+private static long tm(long a,long b){ return (b==0)?0:(a%b); }
+static void Main(string[] args)
+{
+        long t0,t1,t2;
+        gw(1,0,486);
+        gw(2,0,1029);
+        gw(4,0,500094);
+        gw(3,0,2);
+        gw(0,3,32);
+        gw(1,3,32);
+        gw(1,1,1000000);
+        gw(2,1,19);
+        gw(2,2,0);
+        gw(3,2,1);
+        gw(4,2,2);
+        sa(gr(4,0)-1);
+        gw(tm(gr(4,0)-1,gr(1,0)),(td(gr(4,0)-1,gr(1,0)))+3,35);
+    _1:
+        sa(sr());
+        if(sp()!=0)goto _30;else goto _2;
+    _2:
+        gw(tm(gr(3,0),gr(1,0)),(td(gr(3,0),gr(1,0)))+3,88);
+        sp();
+    _3:
+        sa(gr(3,0)+gr(3,0));
+        sa((gr(3,0)+gr(3,0))<gr(4,0)?1:0);
+    _4:
+        if(sp()!=0)goto _29;else goto _5;
+    _5:
+        sp();
+    _6:
+        sa(gr(3,0)+1);
+        gw(3,0,gr(3,0)+1);
+        sa(tm(sr(),gr(1,0)));
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        sa(td(sp(),gr(1,0)));
+        sa(sp()+3L);
+        {long v0=sp();t0=gr(sp(),v0);}
+        t0=t0-32;
+        if((t0)!=0)goto _8;else goto _6;
+    _8:
+        if(gr(4,0)>gr(3,0))goto _9;else goto _10;
+    _9:
+        gw(tm(gr(3,0),gr(1,0)),(td(gr(3,0),gr(1,0)))+3,88);
+        goto _3;
+    _10:
+        gw(3,0,0);
+        sa(0);
+        sa(gr(0,3)-88);
+    _11:
+        if(sp()!=0)goto _12;else goto _28;
+    _12:
+        sa(sp()+1L);
+        if(sr()!=gr(4,0))goto _13;else goto _14;
+    _13:
+        sa(sr());
+        sa(tm(sr(),gr(1,0)));
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        sa(td(sp(),gr(1,0)));
+        sa(sp()+3L);
+        {long v0=sp();sa(gr(sp(),v0));}
+        sa(sp()-88L);
+        goto _11;
+    _14:
+        gw(5,0,gr(3,0));
+        gw(1,2,td((gr(1,1)*gr(1,1))-gr(1,1),2));
+        sp();
+        sa(gr(2,1)-1);
+        gw(gr(2,1)+8,1,32);
+    _15:
+        sa(sr());
+        sa((sp()!=0)?0:1);
+        if(sp()!=0)goto _16;else goto _27;
+    _16:
+        gw(9,1,0);
+        sp();
+    _17:
+        if(gr(4,2)>gr(1,1))goto _19;else goto _18;
+    _18:
+        gw(1,2,gr(3,2)+(gr(1,2)-(gr(4,2)-1)));
+    _19:
+        if((((gr(2,1)-1)>gr(2,2)?1:0)*((gr(tm(gr(gr(2,2)+9,1),gr(1,0)),(td(gr(gr(2,2)+9,1),gr(1,0)))+3)*gr(4,2))<=gr(1,1)?1L:0L))!=0)goto _26;else goto _20;
+    _20:
+        gw(4,2,td(gr(4,2),gr(tm(gr(gr(2,2)+9,1),gr(1,0)),(td(gr(gr(2,2)+9,1),gr(1,0)))+3)));
+        gw(3,2,td(gr(3,2),gr(tm(gr(gr(2,2)+9,1),gr(1,0)),(td(gr(gr(2,2)+9,1),gr(1,0)))+3)-(((gr(2,2)>0?1:0)*((gr(gr(2,2)+9,1)-gr(gr(2,2)+8,1)!=0)?0L:1L)!=0)?0:1)));
+        gw(gr(2,2)+9,1,gr(gr(2,2)+9,1)+1);
+    _21:
+        if(((gr(5,0)>gr(gr(2,2)+9,1)?1:0)*((gr(4,2)*gr(tm(gr(gr(2,2)+9,1),gr(1,0)),(td(gr(gr(2,2)+9,1),gr(1,0)))+3))<=gr(1,1)?1L:0L))!=0)goto _25;else goto _22;
+    _22:
+        gw(gr(2,2)+9,1,32);
+        t0=0;
+        t1=gr(2,2)-1;
+        gw(2,2,gr(2,2)-1);
+        t2=t0>t1?1:0;
+        t2=(t2!=0)?0:1;
+        if((t2)!=0)goto _24;else goto _23;
+    _23:
+        System.Console.Out.Write(gr(1,2));
+        return;
+    _24:
+        t0=gr(tm(gr(gr(2,2)+9,1),gr(1,0)),(td(gr(gr(2,2)+9,1),gr(1,0)))+3);
+        gw(4,2,td(gr(4,2),gr(tm(gr(gr(2,2)+9,1),gr(1,0)),(td(gr(gr(2,2)+9,1),gr(1,0)))+3)));
+        t0=t0-(((gr(2,2)>0?1:0)*((gr(gr(2,2)+9,1)-gr(gr(2,2)+8,1)!=0)?0L:1L)!=0)?0:1);
+        t1=gr(3,2);
+        t2=td(t1,t0);
+        gw(3,2,t2);
+        gw(gr(2,2)+9,1,gr(gr(2,2)+9,1)+1);
+        goto _21;
+    _25:
+        t0=gr(tm(gr(gr(2,2)+9,1),gr(1,0)),(td(gr(gr(2,2)+9,1),gr(1,0)))+3);
+        gw(4,2,gr(tm(gr(gr(2,2)+9,1),gr(1,0)),(td(gr(gr(2,2)+9,1),gr(1,0)))+3)*gr(4,2));
+        t0=t0-1;
+        t0=t0*gr(3,2);
+        gw(3,2,t0);
+        goto _17;
+    _26:
+        sa(gr(2,2));
+        gw(2,2,gr(2,2)+1);
+        sa(sp()+9L);
+        sa(1);
+        {long v0=sp();sa(gr(sp(),v0));}
+        sa(sr());
+        gw(gr(2,2)+9,1,sp());
+        sa(tm(sr(),gr(1,0)));
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        sa(td(sp(),gr(1,0)));
+        sa(sp()+3L);
+        {long v0=sp();t0=gr(sp(),v0);}
+        t1=t0*gr(4,2);
+        gw(4,2,t1);
+        t0=t0*gr(3,2);
+        gw(3,2,t0);
+        goto _17;
+    _27:
+        sa(sp()-1L);
+        sa(sr());
+        sa(32);
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        sa(sp()+9L);
+        sa(1);
+        {long v0=sp();long v1=sp();gw(v1,v0,sp());}
+        goto _15;
+    _28:
+        sa(sr());
+        sa(gr(3,0));
+        gw(3,0,gr(3,0)+1);
+        sa(tm(sr(),gr(1,0)));
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        sa(td(sp(),gr(1,0)));
+        sa(sp()+3L);
+        {long v0=sp();long v1=sp();gw(v1,v0,sp());}
+        goto _12;
+    _29:
+        sa(sr());
+        sa(32);
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        sa(tm(sr(),gr(1,0)));
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        sa(td(sp(),gr(1,0)));
+        sa(sp()+3L);
+        {long v0=sp();long v1=sp();gw(v1,v0,sp());}
+        sa(sp()+gr(3,0));
+        sa(sr()<gr(4,0)?1:0);
+        goto _4;
+    _30:
+        sa(sp()-1L);
+        sa(sr());
+        sa(35);
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        sa(tm(sr(),gr(1,0)));
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        sa(td(sp(),gr(1,0)));
+        sa(sp()+3L);
+        {long v0=sp();long v1=sp();gw(v1,v0,sp());}
+        goto _1;
+}}
