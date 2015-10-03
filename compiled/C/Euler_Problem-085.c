72f8a3ff2b9e63b6bf834a3c97073fef0d1fbac3
Mike Schwörer <mailport@mikescher.de>
2015-10-03 20:46:11 +0200

    Added Solution to Problem [085]

---
+++ compiled/C/Euler_Problem-085.c
@@ -0,0 +1,64 @@
+/* compiled with BefunCompile v1.0.8 (c) 2015 */
+#include <stdio.h>
+#include <stdlib.h>
+#define int64 long long
+int64 td(int64 a,int64 b){ return (b==0)?0:(a/b); }
+int64 tm(int64 a,int64 b){ return (b==0)?0:(a%b); }
+int64*s;int q=16384;int y=0;
+int64 sp(){if(!y)return 0;return s[--y];}
+void sa(int64 v){if(q-y<8)s=(int64*)realloc(s,(q*=2)*sizeof(int64));s[y++]=v;}
+int64 sr(){if(!y)return 0;return s[y-1];}
+int main(void)
+{
+    int64 t0;
+    int64 x0=2000000;
+    int64 x1=2000000;
+    int64 x2=0;
+    int64 x3=89;
+    int64 x4=0;
+    int64 x5=89;
+    int64 x6=89;
+    s=(int64*)calloc(q,sizeof(int64));
+_1:
+    if(x0>x4)goto _3;else goto _2;
+_2:
+    printf("%lld", x3);
+    return 0;
+_3:
+    x5=1;
+    x6=x4;
+_4:
+    if(((x2>x5?1:0)*(x0>x6?1LL:0LL))!=0)goto _5;else goto _12;
+_5:
+    sa(x0-x6);
+    if((x0-x6)<0)goto _11;else goto _6;
+_6:
+    sa(sp()-0LL);
+_7:
+    if(sr()>x1)goto _8;else goto _10;
+_8:
+    sp();
+_9:
+    t0=x5+1;
+    x5++;
+    t0*=x4;
+    t0+=x6;
+    x6=t0;
+    goto _4;
+_10:
+    x1=sp();
+    x3=x5*x2;
+    goto _9;
+_11:
+    t0=0;
+    sa(t0);
+    {int64 v0=sp();int64 v1=sp();sa(v0);sa(v1);}
+    {int64 v0=sp();sa(sp()-v0);}
+    goto _7;
+_12:
+    t0=x2+1;
+    x2++;
+    t0+=x4;
+    x4=t0;
+    goto _1;
+}
---
+++ compiled/CSharp/Euler_Problem-085.cs
@@ -0,0 +1,62 @@
+/* compiled with BefunCompile v1.0.8 (c) 2015 */
+public static class Program 
+{
+private static System.Collections.Generic.Stack<long> s=new System.Collections.Generic.Stack<long>();
+private static long sp(){ return (s.Count==0)?0:s.Pop(); }
+private static void sa(long v){ s.Push(v); }
+private static long sr(){ return (s.Count==0)?0:s.Peek(); }
+private static long td(long a,long b){ return (b==0)?0:(a/b); }
+private static long tm(long a,long b){ return (b==0)?0:(a%b); }
+static void Main(string[] args)
+{
+        long t0;
+        long x0=2000000;
+        long x1=2000000;
+        long x2=0;
+        long x3=89;
+        long x4=0;
+        long x5=89;
+        long x6=89;
+    _1:
+        if(x0>x4)goto _3;else goto _2;
+    _2:
+        System.Console.Out.Write(x3);
+        return;
+    _3:
+        x5=1;
+        x6=x4;
+    _4:
+        if(((x2>x5?1:0)*(x0>x6?1L:0L))!=0)goto _5;else goto _12;
+    _5:
+        sa(x0-x6);
+        if((x0-x6)<0)goto _11;else goto _6;
+    _6:
+        sa(sp()-0L);
+    _7:
+        if(sr()>x1)goto _8;else goto _10;
+    _8:
+        sp();
+    _9:
+        t0=x5+1;
+        x5++;
+        t0*=x4;
+        t0+=x6;
+        x6=t0;
+        goto _4;
+    _10:
+        x1=sp();
+        x3=x5*x2;
+        goto _9;
+    _11:
+        t0=0;
+        sa(t0);
+        {long v0=sp();long v1=sp();sa(v0);sa(v1);}
+        {long v0=sp();sa(sp()-v0);}
+        goto _7;
+    _12:
+        t0=x2+1;
+        x2++;
+        t0+=x4;
+        x4=t0;
+        goto _1;
+}}
